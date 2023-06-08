acf362ae2052df22f034e33d0c96921b2c569344
Siso Ngqolosi <sisoalbert@yahoo.com>
2023-06-09 00:09:39 +0200

    Added realm

--- App.tsx
+++ App.tsx
@@ -1,11 +1,41 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {appId, baseUrl} from './atlasConfig.json';
+import {AppProvider, UserProvider, useApp} from '@realm/react';
 
 const App = () => {
+  const WelcomeView = () => {
+    const app = useApp();
+
+    const handlelogin = async () => {
+      const credentials = Realm.Credentials.anonymous();
+      try {
+        const user = await app.logIn(credentials);
+      } catch (err) {
+        console.error('Failed to log in', err);
+      }
+    };
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Welcome to Atlas</Text>
+      </View>
+    );
+  };
+
+  const MainApp = () => {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Main App</Text>
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <AppProvider id={appId} baseUrl={baseUrl}>
+      <UserProvider fallback={WelcomeView}>
+        <MainApp />
+      </UserProvider>
+    </AppProvider>
   );
 };
 
